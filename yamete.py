a6da4b0fd7f2cad025fbe1acbf1abc604a2f67cd
janw4ld <janw4ld@tutanota.com>
2022-12-08 19:52:26 +0200

    modified lolviz to not follow parent links

--- yamete.py
+++ yamete.py
@@ -50,5 +50,186 @@ def gr_dict_html(title, items, highlight=None, bgcolor=lv.YELLOW, separator="&ra
     return header + blankrow.join(rows) + tail
 
 
+def obj_node(p, varnames=None):
+    s = ""
+    nodename = "node%d" % id(p)
+    if type(p) == lv.types.FrameType:
+        frame = p
+        info = lv.inspect.getframeinfo(frame)
+        caller_scopename = info[2]
+        if caller_scopename == '<module>':
+            caller_scopename = 'globals'
+        argnames, _, _ = lv.inspect.getargs(frame.f_code)
+        items = []
+        # do args first to get proper order
+        for arg in argnames:
+            if varnames is not None and arg not in varnames: continue
+            v = frame.f_locals[arg]
+            if lv.isatom(v):
+                items.append((arg, arg, v))
+            else:
+                items.append((arg, arg, None))
+        for k, v in frame.f_locals.items():
+            if varnames is not None and k not in varnames: continue
+            if k in argnames:
+                continue
+            if lv.ignoresym((k, v)):
+                continue
+            if lv.isatom(v):
+                items.append((k, k, v))
+            else:
+                items.append((k, k, None))
+        s += '// FRAME %s\n' % caller_scopename
+        s += lv.gr_dict_node(nodename, caller_scopename, items, highlight=argnames, bgcolor=BLUE,
+                          separator=None, reprkey=False)
+    elif isinstance(p,dict):
+        # print "DRAW DICT", p, '@ node' + nodename
+        items = []
+        i = 0
+        for k, v in p.items():
+            if varnames is not None and k not in varnames: continue
+            if lv.isatom(v):
+                items.append((str(i), k, v))
+            else:
+                items.append((str(i), k, None))
+            i += 1
+        s += '// DICT\n'
+        s += lv.gr_dict_node(nodename, None, items)
+    elif isinstance(p,set) and len(p) == 0:  # special case "empty set"
+        s += 'node%d [margin="0.03", shape=none label=<<font face="Times-Italic" color="#444443" point-size="9">empty set</font>>];\n' % id(p)
+    elif p is True or p is False:  # Boolean
+        s += 'node%d [margin="0.03", shape=none label=<<font face="Times-Italic" color="#444443" point-size="9">%s</font>>];\n' % (id(p), str(p))
+    elif type(p).__module__ == 'numpy' and type(p).__name__ == 'ndarray':
+        s += lv.gr_ndarray_node('node%d' % id(p), p)
+    elif type(p).__module__ == 'pandas.core.series' and type(p).__name__ == 'Series':
+        s += lv.gr_ndarray_node('node%d' % id(p), p.values)
+    elif type(p).__module__ == 'pandas.core.frame' and type(p).__name__ == 'DataFrame':
+        s += lv.gr_ndarray_node('node%d' % id(p), p.values)
+    elif isinstance(p,list) and len(p)==0: # special case "empty list"
+        s += 'node%d [margin="0.03", shape=none label=<<font face="Times-Italic" color="#444443" point-size="9">empty list</font>>];\n' % id(p)
+    elif hasattr(p, "__iter__") and lv.isatomlist(p) or type(p)==tuple:
+        # print "DRAW LIST", p, '@ node' + nodename
+        elems = []
+        for el in p:
+            if lv.isatom(el):
+                elems.append(el)
+            else:
+                elems.append(None)
+        if isinstance(p,set):
+            s += '// SET of atoms\n'
+            s += lv.gr_set_node(nodename, elems)
+        else:
+            s += '// LIST or ITERATABLE of atoms\n'
+            s += lv.gr_list_node(nodename, elems)
+    elif hasattr(p, "__iter__"):
+        # print "DRAW VERTICAL LIST", p, '@ node' + nodename
+        elems = []
+        for el in p:
+            if lv.isatom(el):
+                elems.append(el)
+            else:
+                elems.append(None)
+        s += '// VERTICAL LIST or ITERATABLE\n'
+        if isinstance(p,set):
+            s += lv.gr_vlol_node(nodename, elems, title='set', showindexes=False)
+        else:
+            s += lv.gr_vlol_node(nodename, elems)
+
+    elif hasattr(p, "__dict__"): # generic object
+        # print "DRAW OBJ", p, '@ node' + nodename
+        items = []
+        for k, v in p.__dict__.items():
+            if k=='parent': continue
+            elif lv.isatom(v):
+                items.append((k, k, v))
+            else:
+                items.append((k, k, None))
+        s += '// %s OBJECT with fields\n' % p.__class__.__name__
+        s += lv.gr_dict_node(nodename, p.__class__.__name__, items, separator=None,
+                          reprkey=False)
+    else:
+        s += 'node%d [margin="0.03", shape=none label=<<font face="Times-Italic" color="#444443" point-size="9">%s</font>>];\n' % (id(p),abbrev_and_escape("<%s:%s>" % (type(p).__name__,repr(p))))
+
+    return s
+
+
+def closure_(p, varnames, visited):
+    if p is None or lv.isatom(p):
+        return []
+    if id(p) in visited:
+        return []
+    visited.add(id(p))
+    result = []
+    if type(p) != lv.types.FrameType:
+        result.append(p)
+
+    # now chase where we can reach
+    if type(p) == lv.types.FrameType:
+        frame = p
+        info = lv.inspect.getframeinfo(frame)
+        for k, v in frame.f_locals.items():
+            # error('INCLUDE frame var %s' % k)
+            if varnames is not None and k not in varnames: continue
+            if not lv.ignoresym((k, v)):
+                cl = closure_(v, varnames, visited)
+                result.extend(cl)
+        caller_scopename = info[2]
+        if caller_scopename != '<module>': # stop at globals
+            cl = closure_(p.f_back, varnames, visited)
+            result.extend(cl)
+    elif type(p).__module__ == 'numpy' and type(p).__name__ == 'ndarray':
+        pass # ndarray added already above; don't chase its elements here
+    elif type(p).__module__ == 'pandas.core.series' and type(p).__name__ == 'Series':
+        pass  # pd.Series added already above; don't chase its elements here
+    elif type(p).__module__ == 'pandas.core.frame' and type(p).__name__ == 'DataFrame':
+        pass
+    elif type(p)==dict:
+        for k,q in p.items():
+            cl = closure_(q, varnames, visited)
+            result.extend(cl)
+    elif hasattr(p, "__dict__"): # regular object like Tree or Node
+        for k,q in p.__dict__.items():
+            if k!='parent':
+                cl = closure_(q, varnames, visited)
+                result.extend(cl)
+    elif hasattr(p, "__iter__"): # a list or similar
+        for q in p:
+            # check if q has a parent attribute
+            cl = closure_(q, varnames, visited)
+            result.extend(cl)
+    return result
+
+
+def node_edges(p, varnames=None):
+    """Return list of (p, fieldname-in-p, q) for all ptrs in p"""
+    edges = []
+    if type(p) == lv.types.FrameType:
+        frame = p
+        for k, v in frame.f_locals.items():
+            if varnames is not None and k not in varnames: continue
+            if not lv.ignoresym((k, v)) and not lv.isatom(v) and v is not None:
+                edges.append((frame, k, v))
+    elif type(p) == dict:
+        i = 0
+        for k, v in p.items():
+            if not lv.isatom(v) and v is not None:
+                edges.append((p, str(i), v))
+            i += 1
+    elif type(p).__module__ == 'numpy' and type(p).__name__ == 'ndarray':
+        pass # don't chase elements
+    elif hasattr(p, "__iter__"):
+        for i, el in enumerate(p):
+            if not lv.isatom(el) and el is not None:
+                edges.append((p, str(i), el))
+    elif hasattr(p, "__dict__"):
+        for k, v in p.__dict__.items():
+            if k == 'parent': continue
+            if not lv.isatom(v) and v is not None:
+                edges.append((p, k, v))
+    return edges
+
+
 # overriding the function inside the module
 lv.gr_dict_html = gr_dict_html
+lv.node_edges = node_edges
+lv.closure_ = closure_

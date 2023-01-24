c5a4d8d0a93b4e41ad73b874095e49a82834f7e7
MoazSalem <moaztalaat6@gmail.com>
2023-01-25 00:37:09 +0200

    Playing with Flet + Web Release
    Project can now be tested online

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# sliding puzzle solver using traditional search
-## [sample output](./Source.gv.pdf)
-## setup instructions
+# Sliding Puzzle Solver using traditional search
+## [Try it out in Web Here](https://flet-sliding-puzzle.fly.dev/)
+## [Sample output](./Source.gv.pdf)
+## Setup Instructions
 1. Install [graphviz](https://gitlab.com/api/v4/projects/4207231/packages/generic/graphviz-releases/7.0.4/windows_10_cmake_Release_graphviz-install-7.0.4-win64.exe), and add it to system PATH
    </br>![add graphviz to windows PATH](README.d/graphviz-path.png)
 2. Install Pipenv, or conda/mamba etc.
@@ -8,7 +9,7 @@
 3. Install project environment & dependencies
    </br>```pipenv install```
 
-## running the project
+## Running the Project
 1. cd to project dir.
 2. Switch to project venv
    </br>```pipenv shell```
--- app.py
+++ app.py
@@ -2,27 +2,32 @@ from flet import *
 from main import np, time, frames, PLACEHOLDER, SLOT, WIDTH as BOARD_WIDTH
 
 def gui(page: Page):
-    index = 0
+    
     page.title = "Sliding Puzzle"
     page.vertical_alignment = "center"
+    page.on_resize = lambda e: blocks_map(update_width)
+    #Define a Starting Window Size
+    page.window_width = 500
+    page.window_height = 720
+    page.window_min_width = 500
+    page.window_min_height = 700
+    #page.appbar = AppBar(title=Text("Sliding Puzzle", color=colors.BACKGROUND, weight="bold"), center_title=True, bgcolor="blue200",toolbar_height=70)
+    index = 0
     buttons = Ref[Text]()
-
-    width_equation = lambda: page.window_width * 0.7/(BOARD_WIDTH+0.7)
+    steps_summary = Ref[Text]()
+    steps_string = lambda: f"Step {index+1} of {len(frames)}"
+    empty = lambda e: True if e!=PLACEHOLDER else False
+    width_equation = lambda: page.window_width * 0.7/(BOARD_WIDTH)
     block_width_or = lambda x, fn=width_equation: dyn if (dyn:=fn()) < x else x
     value = lambda s: s if s!=PLACEHOLDER else SLOT
     FALLBACK_WIDTH = 150
     blocks = [[ # the TextField matrix to display the puzzle
             TextField(
                 value=value(block), text_align="center", dense=True,
-                width=block_width_or(FALLBACK_WIDTH), disabled=True
+                width=block_width_or(FALLBACK_WIDTH), disabled=empty(block), read_only=True, border_color="blue200",
             )
             for block in row
         ] for row in frames[index]]
-
-    page.on_resize = lambda e: blocks_map(update_width)
-
-    steps_summary = Ref[Text]()
-    steps_string = lambda: f"Step {index+1} of {len(frames)}"
 
     def blocks_map(fn):
         nonlocal blocks
@@ -30,13 +35,14 @@ def gui(page: Page):
             fn(x, y, block, blocks)
         page.update()
 
-    button_width = lambda: (BOARD_WIDTH)*block_width_or(FALLBACK_WIDTH)
+    button_width = lambda: (BOARD_WIDTH+0.3)*block_width_or(FALLBACK_WIDTH)
     def update_width(x, y, b, m): 
         m[x][y].width = block_width_or(FALLBACK_WIDTH)
         buttons.current.width = button_width()
 
     def update_value(x, y, b, m):
         m[x][y].value = str(value(b))
+        m[x][y].disabled = empty(b)
 
     def update_board():
         steps_summary.current.value = steps_string()
@@ -48,7 +54,7 @@ def gui(page: Page):
             index += 1
             update_board()
         if index == len(frames) - 1:
-            steps_summary.current.color="green400"
+            steps_summary.current.color="green"
             steps_summary.current.value = "Goal Reached!"
             page.update()
 
@@ -56,7 +62,7 @@ def gui(page: Page):
         nonlocal index
         if index > 0:
             index -= 1
-            steps_summary.current.color="white"
+            steps_summary.current.color="blue200"
             update_board()
     
     def auto_solve(e):
@@ -69,16 +75,16 @@ def gui(page: Page):
             ], alignment="center")
             for row in blocks])
 
-    page.add(Row([Row(ref=buttons, controls=[
-            ElevatedButton("Previous", on_click=prev_frame, expand=1),
-            ElevatedButton("Next", on_click=next_frame, expand=1),
-            ElevatedButton("Auto Solve", on_click=auto_solve, expand=1)
+    page.add(Container(height = 10),Row([Row(ref=buttons, controls=[
+            OutlinedButton("Previous", on_click=prev_frame, expand=1),
+            FilledButton("Next", on_click=next_frame, expand=1),
+            ElevatedButton("Auto Solve", on_click=auto_solve, expand=2)
         ], alignment="center",
         width=button_width())],
         alignment="center"))
 
-    page.add(Row([Text(ref=steps_summary, color="white")], alignment="center"))
+    page.add(Container(height = 5),Row([Text(ref=steps_summary, color="blue200", weight="bold")], alignment="center"))
     steps_summary.current.value = steps_string()
     page.update()
 
-if (frames): app(target=gui, port=8550)
+if (frames): app(target=gui, port=8080)

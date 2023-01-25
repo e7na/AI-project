e58a7c3e40460092990ef1b2240310ae1b913637
MoazSalem <moaztalaat6@gmail.com>
2023-01-25 15:49:43 +0200

    Added Theme Switching + More Bugs

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flet import *
-from main import np, time, frames, PLACEHOLDER, SLOT, WIDTH as BOARD_WIDTH
+from main import np, time, frames, PLACEHOLDER, SLOT, HEIGHT as BOARD_HIGHT , WIDTH as BOARD_WIDTH
 
 
 def gui(page: Page):
@@ -8,13 +8,15 @@ def gui(page: Page):
     page.on_resize = lambda e: blocks_map(update_width)
     # Define a Starting Window Size
     page.window_width = 550
-    page.window_height = 650
+    #This is Not Right:
+    page.window_height = (BOARD_HIGHT * 90) + 120
     page.window_min_width = 550
-    page.window_min_height = 650
-    # page.appbar = AppBar(title=Text("Sliding Puzzle", color=colors.BACKGROUND, weight="bold"), center_title=True, bgcolor="blue200",toolbar_height=70)
+    #page.window_min_height = 650
+    page.theme_mode = "dark"
     index = 0
     buttons = Ref[Text]()
     steps_summary = Ref[Text]()
+    tools_column = Ref[Column]()
     steps_string = lambda: f"{index+1} of {len(frames)}"
     width_equation = lambda: page.window_width * 0.65 / (BOARD_WIDTH)
     block_width_or = lambda x, fn=width_equation: dyn if (dyn := fn()) < x else x
@@ -34,6 +36,7 @@ def gui(page: Page):
         nonlocal blocks
         for (x, y), block in np.ndenumerate(frames[index]):
             fn(x, y, block, blocks)
+        tools_column.current.height = page.window_height
         page.update()
 
     button_width = lambda: (BOARD_WIDTH+0.3)*block_width_or(FALLBACK_WIDTH)
@@ -70,6 +73,21 @@ def gui(page: Page):
         for _ in frames[index::]:
             next_frame(e)
             time.sleep(0.4)
+
+    def change_theme(e):
+        # Switch Between dark and light mode
+        page.theme_mode = "light" if page.theme_mode == "dark" else "dark"
+        theme_icon_button.selected = not theme_icon_button.selected
+        page.update()
+
+    # button to change theme_mode (from dark to light mode, or the reverse)
+    theme_icon_button = IconButton(icons.DARK_MODE, selected_icon=icons.LIGHT_MODE, icon_color=colors.BLACK,
+        icon_size=30, tooltip="change theme",
+        on_click=change_theme,
+        style=ButtonStyle(color={"": colors.BACKGROUND, "selected": colors.WHITE}, ), )
+
+    page.appbar = AppBar(title=Text("Sliding Puzzle", color=colors.BACKGROUND, weight="bold"), center_title=True, bgcolor="blue200",toolbar_height=70,
+    actions=[theme_icon_button],)
             
     page.add(Row([Column([
         *[Row([txt for txt in row], alignment="center") for row in blocks],
@@ -105,23 +123,23 @@ def gui(page: Page):
     ElevatedButton("Import", disabled=True, height=58, width=110, style=ButtonStyle(shape=
     RoundedRectangleBorder(radius=10))),
 
-    ElevatedButton("Randomize", disabled=True, height=59, width=110, style=ButtonStyle(shape=
+    ElevatedButton("Randomize", disabled=True, height=58, width=110, style=ButtonStyle(shape=
     RoundedRectangleBorder(radius=10))),
 
     Container(Column([Text("Action:", weight="bold"),
-    Text("Right")],spacing=3,alignment="center"),height=58),
+    Text("Right")], spacing=3, alignment="center"), height=58),
 
     Container(Column([Text("Heuristic:", weight="bold"),
-    Text("6")],spacing=3,alignment="center"),height=59),
+    Text("6")], spacing=3,alignment="center"), height=58),
 
     Container(Column([Text("Step:", weight="bold"),
-    Text(ref=steps_summary, color="blue200",weight="bold"),
-    ],spacing=3,alignment="center"),height=62),
+    Text(ref=steps_summary, color="blue200", weight="bold"),
+    ],spacing=3,alignment="center"), height=58),
 
-    ElevatedButton("Solve", disabled=True, height=59, width=110, style=ButtonStyle(shape=
+    ElevatedButton("Solve", disabled=True, height=58, width=110, style=ButtonStyle(shape=
     RoundedRectangleBorder(radius=10))),
 
-    ],spacing=7)
+    ],ref=tools_column, height= page.window_height, wrap=True, spacing=8)
     ],alignment="center",vertical_alignment="start"))
 
     steps_summary.current.value = steps_string()
